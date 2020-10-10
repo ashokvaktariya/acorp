e2fd1b6696056b52bcb27d8848d0e7c0c20dbced
AShok Dhkadh <aashudhakad0@gmail.com>
2020-10-10 15:11:01 +0530

    updated 3

--- src/components/main_component/events/EventPicture.js
+++ src/components/main_component/events/EventPicture.js
@@ -5,7 +5,11 @@ class EventPicture extends Component {
     return (
       <>
         <div className="card text-center border-0">
+          <img
+            className=" card-img-top mx-auto img-fluid img-circle d-block"
 
+            alt="datacode.in"
+          />
         </div>
       </>
     );
